4e47318c36dd6cb5e1e5e5bb3e4198bea33801a3
C4ndyFl4mes <swedinisaac@gmail.com>
2025-12-16 10:38:54 +0100

    Added an evaluation of Vue.js.

--- src/views/EvaluationView.vue
+++ src/views/EvaluationView.vue
@@ -1,7 +1,38 @@
 <script setup></script>
 
 <template>
-    <h1 class="text-4xl">Vue.js</h1>
+    <div class="w-fit mx-auto md:m-0">
+        <h1 class="text-4xl mb-2">Vue.js</h1>
+        <article class="w-[95vw] max-w-120">
+            <h2 class="text-3xl">My Thoughts</h2>
+            <div class="flex flex-col gap-4">
+                <p>
+                    It wasn't hard getting into Vue, but how the data should be emitted by the child component to the
+                    parent
+                    component was confusing in the beginning. I watched a <a class="underline text-blue-600"
+                        href="https://youtu.be/Kt2E8nblvXU?si=1FHzbFhvjOXKjgeH">Tutorial</a> that went through the
+                    basics,
+                    then
+                    I looked through the relevant documentation page to page. With effort, I managed to get an
+                    understanding
+                    of
+                    the basics of Vue.
+                </p>
+                <p>
+                    I started a Vue project as in the tutorial above by running npm create vue@latest.
+                </p>
+                <p>
+                    It was really fun to make the BaseTableRow.vue component since doing this (edit/deletion mode,
+                    different
+                    buttons etc) in normal JavaScript is insane. It leaves out having to manually identify what actions
+                    going to be done to what element by what buttons.
+                </p>
+                <p>
+                    I'm sure to use Vue in future projects.
+                </p>
+            </div>
+        </article>
+    </div>
 </template>
 
 <style scoped></style>
